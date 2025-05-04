b74b80d84d1e71d982080391d4af229d7ad432c1
Danila Kovalev <kovallll.e@gmail.com>
2025-05-05 00:12:56 +0300

    refactor: add var host

--- apps/frontend/src/app/api/axiosInstance.ts
+++ apps/frontend/src/app/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import { useAuthStore } from '@/shared/model/store/authStore';
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api/',
+  baseURL: `http://${import.meta.env.BACK_HOST}/api/`,
 });
 
 let isRefreshing = false;
